1ecdc467b0600de93527b1db5af73032bde857da
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-10-20 18:23:22 +0700

    Initial commit

---
+++ Prob1.py
@@ -0,0 +1,7 @@
+def trapezoid(f,a=0,b=1,n=60):
+    # f is the function  f(x)
+    # a is the x=a, start point of the integration
+    # b is the x=b, end point of the integration
+    # n means n trapezoid
+    
+    return 0
