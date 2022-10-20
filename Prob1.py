7742751df287dd38135b9efee8bfd27032cda27f
peerajak-kmutt <111479721+peerajak-kmutt@users.noreply.github.com>
2022-10-20 18:27:11 +0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,7 +1,7 @@
-def trapezoid(f,a=0,b=1,n=60):
+def recursive_trapezoid(f,a=0,b=1,n=60):
     # f is the function  f(x)
     # a is the x=a, start point of the integration
     # b is the x=b, end point of the integration
-    # n means n trapezoid
+    # n means 2^n trapezoid
     
     return 0
